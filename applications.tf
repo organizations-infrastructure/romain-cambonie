05a2c602f3554ba43d69de47d87b2dacb8954805
Romain Cambonie <romain.cambonie@gmail.com>
2023-05-29 18:54:10 +0200

    feat: applied changes from specialized organization

--- applications.tf
+++ applications.tf
@@ -1,9 +1,19 @@
 locals {
+  common-applications-variables = {
+    __PROJECT      = local.project.name
+    __ORGANIZATION = local.project.github_organization
+    __ABOUT        = local.project.about
+  }
+}
+
+locals {
   applications = {
     api = {
       github_repository                    = "api"
       github_repository_topics             = ["api", "node", "fastify", "typescript", "postgresql", "aws", "ecr", "docker"]
       template_repositories                = ["codingones-github-templates/fastify-api", "codingones-github-templates/aws-application-api"]
+      templated_files_variables            = merge(local.common-applications-variables, { __REPOSITORY = "api" })
+      template_fork                        = false
       service                              = "ecr"
       policy                               = local.policies.ecr
       allow_force_pushes_to_default_branch = false
@@ -11,7 +21,9 @@ locals {
     client = {
       github_repository                    = "client"
       github_repository_topics             = ["client", "node", "typescript", "aws", "cloudfront", "cognito"]
-      template_repositories                = ["codingones-github-templates/angular-client", "codingones-github-templates/aws-application-client", ]
+      template_repositories                = ["codingones-github-templates/angular-client", "codingones-github-templates/aws-application-client"]
+      templated_files_variables            = merge(local.common-applications-variables, { __REPOSITORY = "client" })
+      template_fork                        = false
       service                              = "s3-client"
       policy                               = local.policies.client
       allow_force_pushes_to_default_branch = false
@@ -29,12 +41,19 @@ module "applications" {
   terraform_organization  = local.project.terraform_cloud_organization
   project                 = local.project.name
 
-  github_repository                    = each.value.github_repository
-  github_repository_topics             = each.value.github_repository_topics
-  template_repositories                = each.value.template_repositories
-  service                              = each.value.service
-  policy                               = each.value.policy
+  github_repository        = each.value.github_repository
+  github_repository_topics = each.value.github_repository_topics
+
+  template_repositories     = each.value.template_repositories
+  templated_files_variables = each.value.templated_files_variables
+  template_fork             = each.value.template_fork
+
+  service = each.value.service
+  policy  = each.value.policy
+
+  # TODO Replace  local.first_run by if the repo exist or not
   allow_force_pushes_to_default_branch = local.first_run || each.value.allow_force_pushes_to_default_branch
+
 
   providers = {
     github = github
--- services.tf
+++ services.tf
@@ -1,42 +1,65 @@
 locals {
+  common-services-variables = {
+    __PROJECT                = local.project.name
+    __TERRAFORM_ORGANIZATION = local.project.terraform_cloud_organization
+    __ORGANIZATION           = local.project.github_organization
+  }
+}
+
+#data "external" "check_repository_exists" {
+#  for_each = local.services
+#
+#  program = ["./check_repository_exists.sh", var.github_organization, var.github_repository]
+#}
+
+
+locals {
   services = {
     vpc = {
       template                             = "codingones-github-templates/aws-service-vpc"
       deployer_policy                      = local.policies.vpc-infrastructure
       allow_force_pushes_to_default_branch = true
+      templated_files_variables            = merge(local.common-services-variables, { __REPOSITORY = "vpc-infrastructure" })
     }
     domain = {
       template                             = "codingones-github-templates/aws-service-domain"
+      templated_files_variables            = merge(local.common-services-variables, { __REPOSITORY = "domain-infrastructure" })
       deployer_policy                      = local.policies.domain-infrastructure
       allow_force_pushes_to_default_branch = true
     }
     email = {
       template                             = "codingones-github-templates/aws-service-email"
+      templated_files_variables            = merge(local.common-services-variables, { __REPOSITORY = "email-infrastructure" })
       deployer_policy                      = local.policies.email-infrastructure
       allow_force_pushes_to_default_branch = true
     }
     identity = {
       template                             = "codingones-github-templates/aws-service-identity"
+      templated_files_variables            = merge(local.common-services-variables, { __REPOSITORY = "identity-infrastructure" })
       deployer_policy                      = local.policies.identity-infrastructure
       allow_force_pushes_to_default_branch = true
     }
     persistence = {
       template                             = "codingones-github-templates/aws-service-persistence"
+      templated_files_variables            = merge(local.common-services-variables, { __REPOSITORY = "persistence-infrastructure" })
       deployer_policy                      = local.policies.persistence-infrastructure
       allow_force_pushes_to_default_branch = true
     }
     registry = {
       template                             = "codingones-github-templates/aws-service-registry"
+      templated_files_variables            = merge(local.common-services-variables, { __REPOSITORY = "registry-infrastructure" })
       deployer_policy                      = local.policies.registry-infrastructure
       allow_force_pushes_to_default_branch = true
     }
     api = {
       template                             = "codingones-github-templates/aws-service-api"
+      templated_files_variables            = merge(local.common-services-variables, { __REPOSITORY = "api-infrastructure" })
       deployer_policy                      = local.policies.api-infrastructure
       allow_force_pushes_to_default_branch = true
     }
     client = {
       template                             = "codingones-github-templates/aws-service-client"
+      templated_files_variables            = merge(local.common-services-variables, { __REPOSITORY = "client-infrastructure" })
       deployer_policy                      = local.policies.client-infrastructure
       allow_force_pushes_to_default_branch = true
     }
@@ -52,12 +75,15 @@ module "services" {
   github_organization     = local.project.github_organization
   terraform_organization  = local.project.terraform_cloud_organization
 
-  github_repository   = "${each.key}-infrastructure"
-  template_repository = each.value.template
+  github_repository         = "${each.key}-infrastructure"
+  template_repository       = each.value.template
+  templated_files_variables = each.value.templated_files_variables
+  template_fork             = false
 
   project = local.project.name
   service = each.key
 
+  # TODO Replace  local.first_run by if the repo exist or not
   allow_force_pushes_to_default_branch = local.first_run || each.value.allow_force_pushes_to_default_branch
 
   policy = each.value.deployer_policy
